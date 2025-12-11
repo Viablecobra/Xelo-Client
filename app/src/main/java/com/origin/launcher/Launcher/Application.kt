27823a4dbf4664b7213f44dcd83dbba107dd6aa6
vcx <ryansk0987@gmail.com>
2025-12-11 13:27:26 +0530

    update

--- app/src/main/java/com/origin/launcher/Launcher/Application.kt
+++ app/src/main/java/com/origin/launcher/Launcher/Application.kt
@@ -5,6 +5,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import com.google.android.material.color.DynamicColors
+import android.content.Intent
+import android.os.Environment
+import com.origin.launcher.BuildConfig
+import com.origin.launcher.CrashActivity
+import xcrash.ICrashCallback
+import xcrash.XCrash
+import java.io.File
 
 class LauncherApplication : Application() {
 
@@ -12,6 +19,36 @@ class LauncherApplication : Application() {
         super.onCreate()
         context = applicationContext
         preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        
+        val callback: ICrashCallback = ICrashCallback { logPath, emergency ->
+    try {
+        val i = Intent(applicationContext, CrashActivity::class.java).apply {
+            putExtra("LOG_PATH", logPath)
+            putExtra("EMERGENCY", emergency)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+        applicationContext.startActivity(i)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+    XCrash.init(this, XCrash.InitParameters().apply {
+    setAppVersion(BuildConfig.VERSION_NAME)
+    setLogDir(
+        File(
+            Environment.getExternalStorageDirectory(),
+            "games/xelo_client/crash_logs"
+        ).absolutePath
+    )
+    setNativeCallback(callback)
+    setJavaCallback(callback)
+    setAnrCallback(callback)
+    setJavaRethrow(false)
+    setNativeRethrow(false)
+    setAnrRethrow(false)
+}) 
+      
+        
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 
