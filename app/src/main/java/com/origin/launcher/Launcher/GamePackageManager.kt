8c6f91d13ff54dadd3d3cfc842f1906ad189da88
VCX <88580298+Viablecobra@users.noreply.github.com>
2025-12-13 23:41:21 +0530

    Update GamePackageManager.kt

--- app/src/main/java/com/origin/launcher/Launcher/GamePackageManager.kt
+++ app/src/main/java/com/origin/launcher/Launcher/GamePackageManager.kt
@@ -29,8 +29,7 @@ class GamePackageManager private constructor(private val context: Context) {
         "libfmod.so",
         "libMediaDecoders_Android.so",
         "libmaesdk.so",
-        "libminecraftpe.so",
-"libmtbinloader2.so"
+        "libminecraftpe.so"
     )
 
     private val systemLoadLibs = arrayOf(
